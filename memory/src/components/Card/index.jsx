a2d4bf536d9bc8ecb1a59c197b91596497add7ca
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-11-05 16:50:05 +0100

    commit

--- memory/src/components/Card/index.jsx
+++ memory/src/components/Card/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import "./style.css";
+/*
 function Card(props) {
-  return <p>{props.text}</p>;
-}
+  return (
+    <h2>
+      I am a {props.color} {props.brand} {props.model}!
+    </h2>
+  );
+}*/
+
+const Card = ({ card, handleChoice, isFlipped, isMatched }) => {
+  return (
+    <button
+      className={isFlipped || isMatched ? "card flipped" : card}
+      onClick={handleChoice(card)}
+    >
+      <img src={card.icon} className="frontSide" alt={card.name}></img>
+      <img src="./backSide.webp" className="backside" alt={card.name}></img>
+    </button>
+  );
+};
 
 export default Card;
