1b5329226f74e25f454141d435b70b73b382904b
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2025-11-05 14:14:30 +0100

    commit

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Card from "./components/Card";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <p>bonjour</p>
+      <h1>bonjour</h1>
+      <Card text="Je suis une carte" />
+      <Button text="Je suis un bouton" />
     </div>
   );
 }
